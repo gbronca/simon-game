e20a179d8dacca2d73d1aeecd50f6a5d9ccf540e
Giovanni <gbronca@gmail.com>
2018-11-14 20:31:57 +0000

    Updated game logic, fixed few bugs.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const computerSequence = new Array(20);
 const colors = [];
-let playerSequence = [];
+const playerSequence = [];
 let strictMode = false;
 let power = false;
-let victory = false;
 let round;
-let correct = true;
+let count;
+let timerVariable;
+let player = true; // True: Computer, False: Human
 
 const counter = document.querySelector('.game-counter');
 const strictButton = document.querySelector('#strict');
@@ -40,7 +41,116 @@ colorPlayed.forEach((color) => {
   });
 });
 
+function play() {
+  power = false; // prevent human player from clicking whilst computer plays
+  if (count === round) {
+    clearInterval(timerVariable);
+    power = true;
+    player = false;
+  }
+
+  if (player === true) {
+    setTimeout(() => {
+      playAudio(colors[computerSequence[count]]);
+      count += 1;
+    }, 200);
+  }
+}
+
+function resetVariables() {
+  count = 0;
+  playerSequence.length = 0;
+  counter.innerHTML = round;
+  player = true;
+}
+
+function start() {
+  // initialize variables to default state
+  timerVariable = 0;
+  round = 1;
+  resetVariables();
+
+  // generate computer game sequence
+  for (let i = 0; i < computerSequence.length; i += 1) {
+    computerSequence[i] = Math.floor(Math.random() * colors.length);
+  }
+
+  timerVariable = setInterval(play, 800);
+}
+
+// listen to control buttons (strict, start, power) change of status
+strictButton.addEventListener('click', () => {
+  if (strictButton.checked) {
+    strictMode = true;
+  } else {
+    strictMode = false;
+  }
+});
+
+startButton.addEventListener('click', () => {
+  if (power) {
+    startButton.innerHTML = 'Reset';
+    start();
+  }
+});
+
+powerButton.addEventListener('click', () => {
+  if (powerButton.checked) {
+    power = true;
+    counter.innerHTML = '-';
+  } else {
+    power = false;
+    startButton.innerHTML = 'Start';
+    counter.innerHTML = '';
+    clearInterval(timerVariable);
+  }
+});
+
+function winGame() {
+  counter.innerHTML = 'WIN!';
+  power = false;
+}
+
+function check() {
+  if (playerSequence[playerSequence.length - 1] !== computerSequence[playerSequence.length - 1]) {
+    counter.innerHTML = 'XX';
+    setTimeout(() => {
+      counter.innerHTML = round;
+      if (strictMode) {
+        start();
+      } else {
+        player = true;
+        resetVariables();
+        timerVariable = setInterval(play, 800);
+      }
+    }, 800);
+  } else if (playerSequence.length === computerSequence.length) {
+    winGame();
+  } else if (round === playerSequence.length) {
+    round += 1;
+    resetVariables();
+    timerVariable = setInterval(play, 800);
+  }
+}
+
+function played(color) {
+  if (power) {
+    // finds the colour clicked by the player and adds
+    // to the playerSequence array the color position in the colors array.
+    const playedColor = color.srcElement.attributes.id.value;
+    playerSequence.push(colors.indexOf(playedColor));
+
+    // ****** TO UPDATE *************
+
+    check();
+    playAudio(playedColor);
+    if (playerSequence[playerSequence.length - 1] !== computerSequence[playerSequence.length - 1]) {
+      setTimeout(() => {
+      }, 300);
+    }
+  }
+}
+
 // adds event listener to game colours
 const colorEvents = Array.from(queryColors.querySelectorAll('.colors-size'));
 colorEvents.forEach(color => color.addEventListener('click', played));
-
